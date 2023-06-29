abde3782f1ddddb78c3515d35a7f44ae3234af0f
ArturT <mr.arturio@gmail.com>
2023-06-29 17:13:29 -0400

    fix boolean

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,9 +4,9 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-store1 = Store.create(name:'Burnaby', annual_revenue: 300000, mens_apparel: 'True', womens_apparel: 'True')
-store2 = Store.create(name:'Richmond', annual_revenue: 1260000, womens_apparel: 'True')
-store3= Store.create(name:'Gastown', annual_revenue: 190000, mens_apparel: 'True')
+store1 = Store.create(name:'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name:'Richmond', annual_revenue: 1260000, womens_apparel: true)
+store3= Store.create(name:'Gastown', annual_revenue: 190000, mens_apparel: true)
 
 puts Store.count
 
