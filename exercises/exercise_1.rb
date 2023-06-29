eccf154805d879786e92c9a7d0c1f3a839a5d92b
ArturT <mr.arturio@gmail.com>
2023-06-29 16:31:00 -0400

    exercise 1: create class method

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,10 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+store1 = Store.create(name:'Burnaby', annual_revenue: 300000, mens_apparel: 'True', womens_apparel: 'True')
+store2 = Store.create(name:'Richmond', annual_revenue: 1260000, womens_apparel: 'True')
+store3= Store.create(name:'Gastown', annual_revenue: 190000, mens_apparel: 'True')
+
+puts Store.count
+
+
